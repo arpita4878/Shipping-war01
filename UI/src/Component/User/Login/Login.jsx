8843b18f83ef7bd3390eb7e877051d23841c109e
arpita4878 <arpitaseth192@gmail.com>
2025-07-06 18:41:00 +0530

    updation in login component enhance captcha integration.

--- UI/src/Component/User/Login/Login.jsx
+++ UI/src/Component/User/Login/Login.jsx
@@ -1,91 +1,89 @@
 import './Login.css';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { __userapiurl } from '../../../API_URL';
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios';
 //import ReCAPTCHA from "react-google-recaptcha"
 
 function Login() {
-  const recaptchaRef=useRef()
+  const recaptchaRef = useRef()
   const navigate = useNavigate()
-  const [email, setEmail]=useState()
-  const [password, setPassword]=useState()
-  const [output,setOutput]=useState()
-  const [type, setType]  =  useState('password')
-  const [passwordText, setPasswordText]  =  useState("Show Password")
-  const [verify, setVerify]= useState(false)
-  const [error, setError]= useState({})
-   const [captchaText, setCaptchaText] = useState();
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+  const [output, setOutput] = useState()
+  const [type, setType] = useState('password')
+  const [passwordText, setPasswordText] = useState("Show Password")
+  const [error, setError] = useState({})
+  const [captchaText, setCaptchaText] = useState();
   const [userInputCaptcha, setUserInputCaptcha] = useState();
-   const [captchaError, setCaptchaError] = useState();
+  const [captchaError, setCaptchaError] = useState();
 
 
 
-//validations  
-  const validate=()=>{
-    const newError={}
-    
-    if(!email) newError.email='Email is required'
-    else if (!/\S+@\S+\.\S+/.test(email)) newError.email='Invalid email format';
+  //validations  
+  const validate = () => {
+    const newError = {}
 
-    if(!password) newError.password='Password is required'
+    if (!email) newError.email = 'Email is required'
+    else if (!/\S+@\S+\.\S+/.test(email)) newError.email = 'Invalid email format';
+
+    if (!password) newError.password = 'Password is required'
 
     setError(newError)
 
-    return Object.keys(newError).length==0
+    return Object.keys(newError).length == 0
   };
 
 
-  
 
-  const handleSubmit=(e)=>{
+
+  const handleSubmit = (e) => {
     e.preventDefault()
 
-   if(!validate()) return;
- 
-    if(userInputCaptcha!==captchaText){
+    if (!validate()) return;
+
+    if (userInputCaptcha !== captchaText) {
       setCaptchaError("captcha does not match")
       return;
     }
-    
-    const loginDetails={"email":email,"password":password}
-   
-   // console.log("loginDetais:",loginDetails);
 
-    axios.post(__userapiurl+"login",loginDetails).then((response)=>{
-    // console.log(response.data)
-      const user=response.data.userDetails;
-      localStorage.setItem("token",response.data.token);
-      localStorage.setItem("name",user.name);
-      localStorage.setItem("email",user.email);
-      localStorage.setItem("mobile",user.mobile);
-      localStorage.setItem("address",user.address);
-      localStorage.setItem("city",user.city);
-      localStorage.setItem("gender",user.gender);
-      localStorage.setItem("role",user.role);
-      localStorage.setItem("info",user.info);
+    const loginDetails = { "email": email, "password": password }
 
-      (user.role=="admin")?navigate("/admin"):navigate("/user")
+    // console.log("loginDetais:",loginDetails);
 
-    }).catch((error)=>{
+    axios.post(__userapiurl + "login", loginDetails).then((response) => {
+      // console.log(response.data)
+      const user = response.data.userDetails;
+      localStorage.setItem("token", response.data.token);
+      localStorage.setItem("name", user.name);
+      localStorage.setItem("email", user.email);
+      localStorage.setItem("mobile", user.mobile);
+      localStorage.setItem("address", user.address);
+      localStorage.setItem("city", user.city);
+      localStorage.setItem("gender", user.gender);
+      localStorage.setItem("role", user.role);
+      localStorage.setItem("info", user.info);
+
+      (user.role == "admin") ? navigate("/admin") : navigate("/user")
+
+    }).catch((error) => {
       setEmail("");
       setPassword("");
       setUserInputCaptcha('')
-       setCaptchaText(generateCaptcha());
+      setCaptchaText(generateCaptcha());
       // if(recaptchaRef.current) recaptchaRef.current.reset();
-      setOutput("Incorrect Email or Password ",error)
+      setOutput("Incorrect Email or Password ", error)
     });
   }
 
 
-//for  text show password or hide password
-  const  handleToggle =()=>{
-    if(type=='password'){
+  //for  text show password or hide password
+  const handleToggle = () => {
+    if (type == 'password') {
       setType('text')
       setPasswordText('Hide Password')
     }
-    else
-    {
+    else {
       setType('password')
       setPasswordText('Show Password')
     }
@@ -94,76 +92,75 @@ function Login() {
 
 
   //captcha google
- 
-// function onChange(value) {
-//   console.log("Captcha value:", value);
-//    setVerify(true); 
- 
-// }
 
-//captcha number and alphabet
-const generateCaptcha=(length=6)=>{
-  const characters='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-  let result='';
-  for(let i=0;i<length;i++)
-  {
-    result+=characters.charAt(Math.floor(Math.random()*characters.length))
+  // function onChange(value) {
+  //   console.log("Captcha value:", value);
+  //    setVerify(true); 
+
+  // }
+
+  //captcha number and alphabet
+  const generateCaptcha = (length = 6) => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length))
+    }
+    return result;
   }
-  return result;
-}
 
 
-useEffect(()=>{
-  setCaptchaText(generateCaptcha());
-},[])
+  useEffect(() => {
+    setCaptchaText(generateCaptcha());
+  }, [])
 
-const refreshCaptcha=()=>{
-  setCaptchaText(generateCaptcha());
-  setUserInputCaptcha('')
-}
+  const refreshCaptcha = () => {
+    setCaptchaText(generateCaptcha());
+    setUserInputCaptcha('')
+  }
 
 
 
   return (
-   <>
-    <div className="d-flex justify-content-center align-items-center   " style={{ minHeight: '40vh', paddingTop: '10px' }} id='login-box'>
-     
-      <div className="col-10 col-sm-8 col-md-6  mt-5 col-lg-4  ">
+    <>
+      <div className="d-flex justify-content-center align-items-center   " style={{ minHeight: '40vh', paddingTop: '10px' }} id='login-box'>
 
-  <h2 style={{color:"black"}}>{output}</h2>
+        <div className="col-10 col-sm-8 col-md-6  mt-5 col-lg-4  ">
 
-        <div className="login-card ">
-         
-          <form >
-            <h2 className="text-center text-dark mb-4">Login</h2>
-          
-            <div className="form-group mb-3">
-             
-              <input
-                type="email"
-                className="form-control p-3"
-                placeholder="Email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-           {error.email && <small className='text-danger'>{error.email}</small>}
-            </div> 
-           
-            
-            <div className="form-group mb-2">
-            
-              <input
-                type={type}
-                className="form-control p-3"
-                placeholder='password' value={password} onChange={(e)=>setPassword(e.target.value)}/>
-                   
-              {/* //checkbox show password hide password */}
-               <input type="checkbox" className='mt-2 ml-1' onClick={handleToggle} /><label style={{'color':'black','marginLeft':'5px'}}>{passwordText}</label>
-          <br />
-              {error.password && <small className='text-danger ' >{error.password}</small>}            
-            </div>
+          <h2 style={{ color: "black" }}>{output}</h2>
+
+          <div className="login-card ">
+
+            <form >
+              <h2 className="text-center text-dark mb-4">Login</h2>
+
+              <div className="form-group mb-3">
+
+                <input
+                  type="email"
+                  className="form-control p-3"
+                  placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                {error.email && <small className='text-danger'>{error.email}</small>}
+              </div>
 
 
+              <div className="form-group mb-2">
 
-            {/* captcha */}
-            {/* <div className='form-group mb-3'>
+                <input
+                  type={type}
+                  className="form-control p-3"
+                  placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+
+                {/* //checkbox show password hide password */}
+                <input type="checkbox" className='mt-2 ml-1' onClick={handleToggle} /><label style={{ 'color': 'black', 'marginLeft': '5px' }}>{passwordText}</label>
+                <br />
+                {error.password && <small className='text-danger ' >{error.password}</small>}
+              </div>
+
+
+
+              {/* captcha */}
+              {/* <div className='form-group mb-3'>
            <ReCAPTCHA
               ref={recaptchaRef}
               sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
@@ -175,34 +172,34 @@ const refreshCaptcha=()=>{
 
 
               {/* word number captcha */}
-               <div className="form-group mb-3">
+              <div className="form-group mb-3">
 
-            <span  style={{fontSize:'28px',color:'red',letterSpacing:'10px',textDecoration:'line-through'}}>{captchaText}</span>&nbsp;&nbsp; <i class="fa fa-sync" style={{color:'black'}} onClick={refreshCaptcha}></i>
+                <span style={{ fontSize: '28px', color: 'red', letterSpacing: '10px', textDecoration: 'line-through' }}>{captchaText}</span>&nbsp;&nbsp; <i class="fa fa-sync" style={{ color: 'black' }} onClick={refreshCaptcha}></i>
 
-            <input
-                type="text"
-                className="form-control p-3"
-                placeholder="Enter Captcha" value={userInputCaptcha} onChange={(e)=>setUserInputCaptcha(e.target.value)}/>
-            <small className='text-danger'>{captchaError}</small>
-            </div>
-        
+                <input
+                  type="text"
+                  className="form-control p-3"
+                  placeholder="Enter Captcha" value={userInputCaptcha} onChange={(e) => setUserInputCaptcha(e.target.value)} />
+                <small className='text-danger'>{captchaError}</small>
+              </div>
 
 
-            <button
-              className="btn btn-primary w-100 py-2"
-              type="button" onClick={handleSubmit} >
-           
-              Log In
-            </button>
-         
-          </form>
-        
+
+              <button
+                className="btn btn-primary w-100 py-2"
+                type="button" onClick={handleSubmit} >
+
+                Log In
+              </button>
+
+            </form>
+
+          </div>
+          <br /><br />
         </div>
-         <br /><br /> 
+
       </div>
-      
-    </div>
-   
+
     </>
   );
 }
